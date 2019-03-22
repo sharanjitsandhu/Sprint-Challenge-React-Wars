b409b18321edba37bf02e36aa1cda240bb0e57bf
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-03-22 08:43:58 -0700

    Initial commit

--- starwars/src/components/Container.js
+++ starwars/src/components/Container.js
@@ -1,13 +1,15 @@
-
 import React from "react";
 import Card from "./Card";
 import "./StarWars.css";
 
 const Container = props => {
+  console.log(props);
   return (
     <div className="container">
-      {props.traits.map(i => {
-        return <Card attribute={i} />;
+      {props.traits.map((character, i) => {
+        // another way to apply keys
+        //return <Card attribute={character} key={i}/>;
+        return <Card attribute={character} key={character.name} />;
       })}
     </div>
   );
